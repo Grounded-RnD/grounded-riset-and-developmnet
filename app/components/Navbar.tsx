1b98aa8124578aac6f2b27828dade799463e92cb
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-19 13:23:25 +0700

    feat: responsive NavBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,140 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import GLogo from "@/public/logo/main-logo.svg";
+import Link from "next/link";
+import { LinkButton } from "../utilities/Button";
+import XIcon from "../utilities/XIcon";
+
+interface NavbarProps {
+  title: string;
+  href: string;
+}
 
 export default function Navbar() {
-  return <React.Fragment></React.Fragment>;
+  const [activeSection, setActiveSection] = useState("Home");
+  const [sideActive, setSideActive] = useState(false);
+  const links: NavbarProps[] = [
+    {
+      title: "Home",
+      href: "#",
+    },
+    {
+      title: "About",
+      href: "#about",
+    },
+    {
+      title: "Products",
+      href: "#products",
+    },
+    {
+      title: "Our Services",
+      href: "#services",
+    },
+    {
+      title: "Investor Relations",
+      href: "#investor-relations",
+    },
+    {
+      title: "Partner",
+      href: "#partner",
+    },
+    {
+      title: "Join Team",
+      href: "#join-team",
+    },
+  ];
+  const handleScroll = () => {
+    const sections: NodeListOf<HTMLElement> = document.querySelectorAll("section[id]");
+
+    sections.forEach((section) => {
+      const sectionHeight = section.offsetHeight;
+      const sectionTop = section.offsetTop - 50;
+
+      if (window.scrollY > sectionTop && window.scrollY <= sectionTop + sectionHeight) {
+        setActiveSection(section.getAttribute("id")!);
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <React.Fragment>
+      <nav className="bg-dark-50 fixed w-full xl:max-w-[1632px] lg:max-w-[1000px] md:max-w-[720px] sm:max-w-[620px] max-w-[480px] mx-auto z-20 top-8 left-0 right-0 rounded-[16px] shadow-sm">
+        <div className="max-w-full flex flex-wrap items-center justify-between p-2 mx-4">
+          <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <Image src={GLogo} className="h-8" alt="Grounded Logo" />
+          </Link>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <LinkButton href="#" variant="primary" className="hidden xl:block">
+              Contact Us
+            </LinkButton>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg xl:hidden hover:bg-warning-400 focus:outline-none focus:ring-2 focus:ring-dark-400"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+              onClick={() => setSideActive(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+          </div>
+          <div className="items-center justify-between hidden w-full xl:flex xl:w-auto xl:order-1" id="navbar-sticky">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 ">
+              {links.map((link, i) => (
+                <li key={i}>
+                  <Link
+                    href={link.href}
+                    className={`inline-block py-2 px-3 font-normal hover:font-medium text-warning-200 hover:text-surface-50 relative text-transition duration-300 after:absolute after:bottom-1 after:left-1/2 after:h-[2px] after:origin-center after:-translate-x-1/2 after:bg-surface-50 after:transition-all after:duration-300 hover:after:w-4/5 ${
+                      activeSection === link.title.toLowerCase() ? "after:w-4/5" : "after:w-0"
+                    }`}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* mobile sidebar */}
+      <aside className="fixed left-0 top-0 z-[1000] h-screen w-[264px] bg-black transition-all duration-300 xl:hidden" style={{ left: sideActive ? 0 : "-100%" }}>
+        <div className="flex h-full flex-col justify-between px-5 py-[42px]">
+          <div className="block">
+            <div className="flex-col">
+              <button className="block h-6 w-6" onClick={() => setSideActive(false)}>
+                <XIcon />
+              </button>
+              <Image src={GLogo} className="h-6 -ml-2 mt-10 mb-[40px] " alt="Grounded Logo" />
+            </div>
+            <ul className="flex flex-col gap-7">
+              {links.map((link, i) => (
+                <li key={i} className="ml-3">
+                  <Link
+                    href={link.href}
+                    className={`relative text-white transition duration-300 before:absolute before:-left-3 before:w-[2px] before:bg-white before:transition-all before:duration-300 hover:before:h-full ${
+                      activeSection === link.title.toLowerCase() ? "before:h-full" : "before:h-0"
+                    }`}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <LinkButton href="#" variant="primary" className="w-fit absolute bottom-10">
+              Contact Us
+            </LinkButton>
+          </div>
+        </div>
+      </aside>
+    </React.Fragment>
+  );
 }
