6736c1d7cd17d0441a744221a3e13e3e0e2a22fc
Haza Nasrullah <hazanasrullah@gmail.com>
2024-06-19 14:20:35 +0700

    feat: add responsif UI to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,28 +11,15 @@ import SkillDataScience from '@/./public/skill/data-science.svg';
 
 const ProfilePage = () => {
   return (
-    <div className="min-h-screen bg-dark-50 text-surface-50">
-      <header className="bg-dark-400 py-4 px-8 flex justify-between items-center">
-        <div className="text-3xl font-bold">grounded.</div>
-        <nav className="space-x-4">
-          <a href="#" className="hover:underline">Home</a>
-          <a href="#" className="hover:underline">About</a>
-          <a href="#" className="hover:underline">Products</a>
-          <a href="#" className="hover:underline">Our Services</a>
-          <a href="#" className="hover:underline">Investor Relation</a>
-          <a href="#" className="hover:underline">Partner</a>
-          <a href="#" className="hover:underline">Join Team</a>
-        </nav>
-        <button className="bg-oranges-300 px-4 py-2 rounded text-dark-50">Contact Us</button>
-      </header>
-      <main className="p-8 grid grid-cols-1 md:grid-cols-3 gap-8">
-        <section className="col-span-1 bg-dark-300 p-6 rounded-2xl flex flex-col items-center justify-center">
-          <Image src={Haza} alt="Profile" className="rounded-full w-[206px] mx-auto" />
-          <h2 className="text-center font-semibold text-[32px] mt-4">Haza Nasrullah K.</h2>
-          <i> <p className="text-center text-[24px] opacity-60">Chief Financial Officer<br />Mobile Developer</p></i>
+    <div className="min-h-screen bg-dark-300 text-surface-50">
+      
+      <main className="p-4 md:p-8 grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+        <section className="col-span-1 bg-dark-50 p-6 rounded-2xl flex flex-col items-center justify-center">
+          <Image src={Haza} alt="Profile" className="rounded-full w-[150px] md:w-[206px] mx-auto" />
+          <h2 className="text-center font-semibold text-[24px] md:text-[32px] mt-4">Haza Nasrullah K.</h2>
+          <i><p className="text-center text-[16px] md:text-[24px] opacity-60">Chief Financial Officer<br />Mobile Developer</p></i>
         </section>
-        <section className="col-span-2 bg-dark-300 p-6 rounded-2xl">
-          {/* mulai tabel y */}
+        <section className="col-span-1 md:col-span-2 bg-dark-50 p-6 rounded-2xl">
           <div className="flex flex-col">
             <div className="-m-1.5 overflow-x-auto">
               <div className="p-1.5 min-w-full inline-block align-middle">
@@ -40,28 +27,28 @@ const ProfilePage = () => {
                   <table className="min-w-full divide-y divide-gray-200">
                     <tbody className="divide-y divide-zinc-700">
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Full Name</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">Haza Nasrullah Kuswantoro</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Full Name</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">Haza Nasrullah Kuswantoro</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Email</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">haza@grounded.org</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Email</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">haza@grounded.org</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Phone</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">62 8962 3084 881</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Phone</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">62 8962 3084 881</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Addres</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">Malang City, East Java, Indonesia</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Address</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">Malang City, East Java, Indonesia</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Member Since</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">August 2023</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Member Since</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">August 2023</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-medium ">Role</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">Co-Founder</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-medium">Role</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[18px] md:text-[24px] font-light">Co-Founder</td>
                       </tr>
                     </tbody>
                   </table>
@@ -70,7 +57,7 @@ const ProfilePage = () => {
             </div>
           </div>
         </section>
-        <section className="col-span-1 bg-dark-300 p-6 rounded">
+        <section className="col-span-1 bg-dark-50 p-6 rounded-2xl ">
           <div className="flex flex-col">
             <div className="-m-1.5 overflow-x-auto">
               <div className="p-1.5 min-w-full inline-block align-middle">
@@ -78,20 +65,20 @@ const ProfilePage = () => {
                   <table className="min-w-full divide-y divide-gray-200">
                     <tbody className="divide-y divide-zinc-700">
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={WebsiteIcon} alt="Website" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">haza.grounded.org</td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={WebsiteIcon} alt="Website" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light">haza.grounded.org</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={LinkedInIcon} alt="LinkedIn" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">haza_nasrullah</td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={LinkedInIcon} alt="LinkedIn" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light">haza_nasrullah</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={InstagramIcon} alt="Instagram" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">@hazanasrullah</td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={InstagramIcon} alt="Instagram" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light">@hazanasrullah</td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={GithubIcon} alt="Github" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light">hazz01</td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={GithubIcon} alt="Github" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light">hazz01</td>
                       </tr>
                     </tbody>
                   </table>
@@ -100,8 +87,8 @@ const ProfilePage = () => {
             </div>
           </div>
         </section>
-        <section className="col-span-1 bg-dark-300 p-6 rounded">
-          <h2 className="text-2xl font-normal mb-4">Techstack & Achievement</h2>
+        <section className="col-span-1 bg-dark-50 p-6 rounded-2xl ">
+          <h2 className="text-xl md:text-2xl font-normal mb-4">Techstack & Achievement</h2>
           <div className="flex flex-col">
             <div className="-m-1.5 overflow-x-auto">
               <div className="p-1.5 min-w-full inline-block align-middle">
@@ -109,18 +96,17 @@ const ProfilePage = () => {
                   <table className="min-w-full divide-y divide-gray-200">
                     <tbody className="divide-y divide-zinc-700">
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillWebFront} alt="Website" className="w-[33px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light"><p className="text-[16px]">FrontEnd Developer</p><p className="text-[12px]">HTML, CSS, React, Tailwind, and 5+</p></td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillWebFront} alt="Website" className="w-[25px] md:w-[33px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light"><p className="text-[16px] md:text-[16px]">FrontEnd Developer</p><p className="text-[10px] md:text-[12px]">HTML, CSS, React, Tailwind, and 5+</p></td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillMobile} alt="LinkedIn" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light"><p className="text-[16px]">Android 7 Ios Developer</p><p className="text-[12px]">Dart, Flutter, Firebase</p></td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillMobile} alt="LinkedIn" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light"><p className="text-[16px] md:text-[16px]">Android & iOS Developer</p><p className="text-[10px] md:text-[12px]">Dart, Flutter, Firebase</p></td>
                       </tr>
                       <tr>
-                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillDataScience} alt="Instagram" className="w-[28px] mx-auto" /></td>
-                        <td className="px-6 py-4 whitespace-nowrap text-[24px] font-light"><p className="text-[16px]">Data Science</p><p className="text-[12px]">Orange Data Science</p></td>
+                        <td className="px-6 py-4 whitespace-nowrap"><Image src={SkillDataScience} alt="Instagram" className="w-[20px] md:w-[28px] mx-auto" /></td>
+                        <td className="px-6 py-4 whitespace-nowrap text-[20px] md:text-[24px] font-light"><p className="text-[16px] md:text-[16px]">Data Science</p><p className="text-[10px] md:text-[12px]">Orange Data Science</p></td>
                       </tr>
-
                     </tbody>
                   </table>
                 </div>
@@ -128,12 +114,12 @@ const ProfilePage = () => {
             </div>
           </div>
         </section>
-        <section className="col-span-1 bg-dark-300 p-6 rounded">
-          <h2 className="text-2xl mb-4">Experience</h2>
-          <p className="text-[16px]">1. Programmer of MokletDev</p>
+        <section className="col-span-1 bg-dark-50 p-6 rounded-2xl ">
+          <h2 className="text-xl md:text-2xl mb-4">Experience</h2>
+          <p className="text-[16px] md:text-[19px]">1. Programmer of MokletDev</p>
         </section>
       </main>
-      <footer className="bg-dark-400 py-4 text-center">
+      <footer className="bg-black py-4 text-center">
         <div>© 2024 Grounded Team</div>
       </footer>
     </div>
