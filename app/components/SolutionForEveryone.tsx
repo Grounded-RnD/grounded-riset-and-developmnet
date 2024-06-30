4ac49b3021bf161e898222635b2a3ab8bb89bcfb
Haza Nasrullah <hazanasrullah@gmail.com>
2024-06-30 14:17:13 +0700

    chore: Update npm dependencies and add Three.js and Three Globe libraries

--- app/components/SolutionForEveryone.tsx
+++ app/components/SolutionForEveryone.tsx
@@ -4,6 +4,8 @@ import AgroSky from "@/public/logo/agrosky.svg";
 import NexaLab from "@/public/logo/nexalab.svg";
 import SmartCounselling from "@/public/logo/smart-counselling.svg";
 import Smartaca from "@/public/logo/smartaca.svg";
+import ThreeGlobe from "./ThreeGlobe";
+
 
 export default function SolutionForEveryone() {
   return (
@@ -45,8 +47,7 @@ export default function SolutionForEveryone() {
 
             <div className="flex items-center justify-center lg:justify-end">
               <div className="relative w-full h-full lg:h-[400px] lg:w-[400px]">
-                {/* Replace the src with the path to your globe image */}
-                <Image src="/path_to_globe_image" alt="Globe" layout="fill" objectFit="contain" />
+                <ThreeGlobe />
               </div>
             </div>
           </div>
