19375e6e4a9dd5b3ec5e9d78848eb99b40b2dc52
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-07-13 14:57:55 +0700

    a

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+const prisma = new PrismaClient();
 
-const globalForPrisma = global as unknown as { db: PrismaClient };
-export const prisma = globalForPrisma.db || db;
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.db = prisma;
-}
 export default prisma;
